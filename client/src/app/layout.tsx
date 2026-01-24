78d70ba501f35dbcb3136b789730618ee61e88fc
jackHotch <jhotchkiss2021@gmail.com>
2026-01-24 18:37:36 -0500

    create basic next and clerk project

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Student Loan Calc',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ client/src/components/header.tsx
@@ -0,0 +1,19 @@
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+export const Header = () => {
+  return (
+    <header className='flex justify-end items-center p-4 gap-4 h-16'>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className='bg-[#6c47ff] text-white rounded font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer'>
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  )
+}
