224164e8f6244f69e39c67684cc82090068bf156
jackHotch <jhotchkiss2021@gmail.com>
2026-02-04 23:20:03 -0500

    added files for loans endpoints

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProfileModule } from './profile/profile.module';
 import { WebhookModule } from './webhook/webhook.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { LoansModule } from './loans/loans.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProfileModule, WebhookModule, UsersModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    WebhookModule,
+    UsersModule,
+    DatabaseModule,
+    LoansModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
